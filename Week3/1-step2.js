77a328ed491977161d4a8fd9399f55ba46f439a7
Sheliann Olsson <48160897+christi023@users.noreply.github.com>
2019-06-06 13:24:18 +0200

    Update 1-step2.js

--- Week3/1-step2.js
+++ Week3/1-step2.js
@@ -2,7 +2,7 @@
 // 2.1 We saw that we can pass functions as arguments to other functions. Your task is to write a function that takes another function as an argument and runs it.
 'use strict';
 function foo(func) {
-  return func;
+  func();
 }
 
 function bar() {
