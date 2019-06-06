c9aafbede5fdfff7469dd4d440e349cb80c2a085
Sheliann Olsson <48160897+christi023@users.noreply.github.com>
2019-06-06 13:27:59 +0200

    Update 2-step2.js

--- Week3/2-step2.js
+++ Week3/2-step2.js
@@ -20,6 +20,23 @@ function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
   for (let i = startIndex; i <= stopIndex; i++) {
     numbers.push(i);
   }
+
+  numbers.forEach(number => {
+    if (number % 3 === 0) {
+      threeCallback(number);
+    }
+    if (number % 5 === 0) {
+      fiveCallback(number);
+    }
+  });
+}
+
+function sayThree(number) {
+  console.log('Number is divisible by 3: ' + number);
+}
+
+function sayFive(number) {
+  console.log('Number is divisible by 5: ' + number);
 }
 
 threeFive(10, 15, sayThree, sayFive);
